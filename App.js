3ccbf7a34050e5df9c976360905d7690a7846336
rahul singh <116128750+rahulroyalsingh@users.noreply.github.com>
2023-12-05 17:20:12 +0530

    ref can manipulate DOM
    By changing color, and hiding items in the DOM.
    It is used for emergency purposes.
    We have to use it very little.

---
+++ App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React,{createRef} from 'react';
+class App extends React.Component{
+  
+  constructor(){
+    super();
+    this.inputRef=createRef();
+  }
+  componentDidMount(){
+    //console.warn(this.inputRef.current.balue='4587')
+  }
+  getVal()
+  {
+    console.warn(this.inputRef.current.value)
+    this.inputRef.current.style.color='red'
+    this.inputRef.current.style.backgroundColor='black'
+  }
+    render()
+  {
+    return (
+      <div className='App'>
+      <h1>Ref in React </h1>
+      <input type='text' ref={this.inputRef} />
+      <button onClick={()=>this.getVal()}>Check Ref</button>
+      </div>
+
+
+    )
+  }
+}
+
+
+export default App;
